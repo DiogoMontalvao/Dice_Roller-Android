6c246b7ec0add3e1529e25b661b8d44b3f4b28d1
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2023-06-27 11:29:31 -0300

    a

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,19 +11,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val numeroLadosDado: Int = 6
+        val numeroLadosDado1: Int = 6
+        val numeroLadosDado2: Int = 20
 
-        val textViewTituloLadosDado: TextView = (findViewById(R.id.textViewTituloLadosDado))
-        textViewTituloLadosDado.text = "Dado de ${numeroLadosDado} Lados"
+        val textViewTituloLadosDado1: TextView = findViewById(R.id.textViewTituloLadosDado1)
+        textViewTituloLadosDado1.text = "Dado de ${numeroLadosDado1} Lados"
+        val textViewTituloLadosDado2: TextView = findViewById(R.id.textViewTituloLadosDado2)
+        textViewTituloLadosDado2.text = "Dado de ${numeroLadosDado2} Lados"
 
         val botaoRolarDado: Button = findViewById(R.id.botaoRolarDado)
-        botaoRolarDado.setOnClickListener { rolarDado(numeroLadosDado) }
+        botaoRolarDado.setOnClickListener { rolarDados(numeroLadosDado1, numeroLadosDado2) }
     }
 
-    private fun rolarDado(numeroLadosDado: Int) {
+    private fun rolarDados(numeroLadosDado1: Int, numeroLadosDado2: Int) {
 
-        val rolarDado = Dado(numeroLadosDado).rolar
-        val textViewResultadoRolagem: TextView = findViewById(R.id.textViewResultadoRolagem)
-        textViewResultadoRolagem.text = rolarDado.toString()
+        fun rolarDado1(): Int {return Dado(numeroLadosDado1).rolar }
+        val textViewResultadoRolagem1: TextView = findViewById(R.id.textViewResultadoRolagem1)
+        textViewResultadoRolagem1.text = rolarDado1().toString()
+
+        fun rolarDado2(): Int { return Dado(numeroLadosDado2).rolar }
+        val textViewResultadoRolagem2: TextView = findViewById(R.id.textViewResultadoRolagem2)
+        textViewResultadoRolagem2.text = rolarDado2().toString()
     }
 }
